e91ebe53a755193ed50463ef7cbb9f7b0bd12c18
Minhhien610 <133361533+Minhhien610@users.noreply.github.com>
2023-05-12 20:34:16 +0700

    add all my files

---
+++ buoi18lop/js/data.js
@@ -0,0 +1,10 @@
+const category=[
+    {cat_id:'phone', cat_name:'Dien thoai'},
+    {cat_id:'laptop', cat_name:'May tinh'},
+    {cat_id:'ram', cat_name:'Bo nho trong'},
+];
+
+const product=[
+    { id:1, name:'sasumg-galaxy-z-fold-4',
+    price:30000000}
+]
